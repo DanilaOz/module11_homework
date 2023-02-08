748907c0214f09940d38927f1c4315836cb508b1
Danila Kolchanov <79622915+DanilaOz@users.noreply.github.com>
2023-02-08 21:19:18 +0500

    Update repeatWord.test.js

--- src/__tests__/utils/repeatWord.test.js
+++ src/__tests__/utils/repeatWord.test.js
@@ -6,7 +6,7 @@ describe("", () => {
     const count = 3;
 
     it("The entered data is correct", () => {
-        expect(repeatWord(word, count)).toBe('word1,word2,word3,');
+        expect(repeatWord(word, count)).toBe('word1,word2,word3');
     }),
     it("The number of word repetitions is less than 1", () => {
         expect(repeatWord(word, 0)).toBe('Значение count должно быть больше 0');
